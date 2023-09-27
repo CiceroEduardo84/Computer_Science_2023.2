aa866f93628b71cd83c607a65d6a4af6d95948a5
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-27 14:48:36 -0300

    add module-02 in the 02-listas-pilha... in the theme estructura de dados

---
+++ Estrutura de dados-Py/02-listas-pilhas-filas-deques/Module-01/listProd.py
@@ -0,0 +1,14 @@
+# Lista Cadastral
+
+# Busca percorre sequencialmente - O(n)
+# Inserção pode ser feita sempre ao final - O(1)
+# variavel armazena sempre o ultimo índice
+# Remoção depende de busca - O(n)
+compras = [[15, 'laranja', 3], [2, 'palmito', 20], [35, 'feijão', 5]]
+
+# Lista Ordenada
+
+# Busca percorre sequencialmente - O(n)
+# Inserção deve manter a ordenação - O(n)
+# Remoção depende de busca - O(n)
+compras2 = [[2, 'palmito', 20], [15, 'laranja', 3], [35, 'feijão', 5]]
